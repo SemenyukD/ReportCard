bc664aa38e4cdd5415a43174ccce0bf864359fdc
SemenyukD <42452896+SemenyukD@users.noreply.github.com>
2018-08-27 14:14:28 +0300

    Create ReportCard2.java

---
+++ ReportCard2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import javax.swing.JOptionPane;
+
+public class ReportCard2 {
+
+	int i;
+	String n1;
+
+	public void scan () {
+		n1 = JOptionPane.showInputDialog("Введіть Вашу оцінку");
+				
+		//i = n1.charAt(i);
+				
+	}
+
+	public void scan1 (char convertGrade) {
+	JOptionPane.showMessageDialog(null, convertGrade);
+	}
+	
+}
+	
